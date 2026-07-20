544bc561b3fb5784dd4d0fb3ccb1c1b8e3e0d109
eldric12 <linnkhantsithu42@gmail.com>
2026-07-20 23:23:16 +0800

    fix(web): initialize SQLite database factory

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+
 import 'app/app.dart';
-import 'package:device_preview/device_preview.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
